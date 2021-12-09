3d8c7635db1c6e92c08ae8bff1a9f90a8c2a5a27
tufoli <m1@tufoli.local>
2021-12-09 09:10:23 +0800

    完成体脂率

--- 1202/fatrare/main.go
+++ 1202/fatrare/main.go
@@ -12,17 +12,17 @@ type human struct {
 	Age      int
 	Gender   string
 	Bmi      float64
-	IsBreak	 bool
-	FatRare  float64
+	IsBreak  bool
+	FatRate  float64
 	FatLevel string
 }
 
 var (
 	genderIndex = map[string]int{
-		"男": 1,
-		"女": 0,
+		"男": 2,
+		"女": 1,
 	}
-	fatRates struct{
+	fatRates struct {
 		Sum float64
 		len int
 	}
@@ -38,11 +38,11 @@ func main() {
 			break
 		}
 	}
-	fmt.Printf("\n共有%v人,%v人的平均体脂率值为:%v\n", fatRates.len, fatRates.len, fatRates.Sum / float64(fatRates.len))
+	fmt.Printf("\n共录入%v人,%v人的平均体脂率值为:%v\n", fatRates.len, fatRates.len, fatRates.Sum/float64(fatRates.len))
 }
 
 func (h *human) readHumanBodyIndex(i int) float64 {
-	fmt.Printf("\n正在输入第%v个人,\n请输入姓名:", i+1)
+	fmt.Printf("\n正在录入第%v个人,\n请输入姓名:", i+1)
 	_, _ = fmt.Scanln(&h.Name)
 	fmt.Print("请输入身高(cm):")
 	_, _ = fmt.Scanln(&h.High)
@@ -59,14 +59,106 @@ func (h *human) readHumanBodyIndex(i int) float64 {
 		h.Gender = "男"
 		genderWeight = genderIndex[h.Gender]
 	}
-	h.FatRare = (1.2*h.Bmi + 0.23*(float64(h.Age)) - 5.4 - 10.8*float64(genderWeight)) / 100
+	h.FatRate = (1.2*h.Bmi + 0.23*(float64(h.Age)) - 5.4 - 10.8*float64(genderWeight)) / 100
 	fmt.Printf("%s输入完成\n", h.Name)
-	fmt.Printf("%s的BMI是:%v,体脂率是%v\n", h.Name, h.Bmi, h.FatRare)//todo 建议:体脂率对照表
+	fmt.Printf("%s的BMI是:%v,体脂率是%v,%s\n", h.Name, h.Bmi, h.FatRate, h.getBodyTips())
 	var whetherContinue string
 	fmt.Print("是否录入下一个(y/n):")
 	_, _ = fmt.Scanln(&whetherContinue)
 	if whetherContinue != "y" {
 		h.IsBreak = true
 	}
-	return h.FatRare
+	return h.FatRate
+}
+
+func (h *human) getBodyTips() string {
+	var (
+		thin = "体态:偏瘦。要多吃多锻炼,增强体质。"
+		fit = "体态:标准。太棒了,要保持哦"
+		overWeight = "体态:偏胖。吃完饭多散散步,消化消化"
+		fat = "体态:肥胖。少吃点,多运动运动"
+		veryFat = "体态:非常肥胖。健身游泳跑步,即刻开始"
+		unknown = "我们不看未成年人的体脂率,变化太大,无法判断"
+	)
+	if h.Age < 18 {
+		return unknown
+	}
+	if h.Gender == "男" {
+		if h.Age >= 18 && h.Age <= 39 {
+			if h.FatRate <= 0.1 {
+				return thin
+			} else if h.FatRate > 0.1 && h.FatRate <= 0.16 {
+				return fit
+			} else if h.FatRate > 0.16 && h.FatRate <= 0.21 {
+				return overWeight
+			} else if h.FatRate > 0.21 && h.FatRate <= 0.26 {
+				return fat
+			} else {
+				return veryFat
+			}
+		} else if h.Age >= 40 && h.Age <= 59 {
+			if h.FatRate <= 0.11 {
+				return thin
+			} else if h.FatRate > 0.11 && h.FatRate <= 0.17 {
+				return fit
+			} else if h.FatRate > 0.17 && h.FatRate <= 0.22 {
+				return overWeight
+			} else if h.FatRate > 0.22 && h.FatRate <= 0.27 {
+				return fat
+			} else {
+				return veryFat
+			}
+		} else {
+			if h.FatRate <= 0.13 {
+				return thin
+			} else if h.FatRate > 0.13 && h.FatRate <= 0.19 {
+				return fit
+			} else if h.FatRate > 0.19 && h.FatRate <= 0.24 {
+				return overWeight
+			} else if h.FatRate > 0.24 && h.FatRate <= 0.29 {
+				return fat
+			} else {
+				return veryFat
+			}
+		}
+	} else {
+		if h.Age >= 18 && h.Age <= 39 {
+			if h.FatRate <= 0.2 {
+				return thin
+			} else if h.FatRate > 0.2 && h.FatRate <= 0.27 {
+				return fit
+			} else if h.FatRate > 0.27 && h.FatRate <= 0.34 {
+				return overWeight
+			} else if h.FatRate > 0.34 && h.FatRate <= 0.39 {
+				return fat
+			} else {
+				return veryFat
+			}
+		} else if h.Age >= 40 && h.Age <= 59 {
+			if h.FatRate <= 0.21 {
+				return thin
+			} else if h.FatRate > 0.21 && h.FatRate <= 0.28 {
+				return fit
+			} else if h.FatRate > 0.28 && h.FatRate <= 0.35 {
+				return overWeight
+			} else if h.FatRate > 0.35 && h.FatRate <= 0.40 {
+				return fat
+			} else {
+				return veryFat
+			}
+		} else {
+			if h.FatRate <= 0.22 {
+				return thin
+			} else if h.FatRate > 0.22 && h.FatRate <= 0.29 {
+				return fit
+			} else if h.FatRate > 0.29 && h.FatRate <= 0.36 {
+				return overWeight
+			} else if h.FatRate > 0.36 && h.FatRate <= 0.41 {
+				return fat
+			} else {
+				return veryFat
+			}
+		}
+	}
+
 }
