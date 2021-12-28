86fee204ef2dd16774712fed336f0e8bc65a7aec
tufoli <m1@tufoli.local>
2021-12-28 16:51:23 +0800

    define error

--- 1223/02-fatRate/Receiver.go
+++ 1223/02-fatRate/Receiver.go
@@ -11,10 +11,16 @@ type Receiver struct {
 	IsBreak bool
 }
 
-var genders = map[string]bool{
-	"male":   true,
-	"female": true,
-}
+var (
+	genders = map[string]bool{
+		"male":   true,
+		"female": true,
+	}
+	IllegalHeightError = errors.New("身高不能小于0")
+	IllegalWeightError = errors.New("体重不能小于0")
+	IllegalAgeError    = errors.New("年龄的范围只能在0-100")
+	IllegalGenderError = errors.New("性别只能为male/female")
+)
 
 func (r *Receiver) receiveArgs() (*BMR.Person, error) {
 	p := new(BMR.Person)
@@ -23,23 +29,23 @@ func (r *Receiver) receiveArgs() (*BMR.Person, error) {
 	fmt.Print("请输入身高(cm):")
 	_, _ = fmt.Scanln(&p.Height)
 	if p.Height <= 0 {
-		return nil, errors.New("身高不能小于0")
+		return nil, IllegalHeightError
 	}
 	fmt.Print("请输入体重(kg):")
 	_, _ = fmt.Scanln(&p.Weight)
 	if p.Weight <= 0 {
-		return nil, errors.New("体重不能小于0")
+		return nil, IllegalWeightError
 	}
 	fmt.Print("请输入年龄(age):")
 	_, _ = fmt.Scanln(&p.Age)
 	if p.Age < 0 || p.Age > 150 {
-		return nil, errors.New("非法的年龄")
+		return nil, IllegalAgeError
 	}
 	fmt.Print("请输入性别(male/female):")
 	_, _ = fmt.Scanln(&p.Gender)
 	_, ok := genders[p.Gender]
 	if !ok {
-		return nil, errors.New(fmt.Sprintf("性别不能为%s", p.Gender))
+		return nil, fmt.Errorf(fmt.Sprintf("性别不能为%s", p.Gender), IllegalGenderError)
 	}
 	fmt.Printf("%s输入完成\n", p.Name)
 	r.Loop++
